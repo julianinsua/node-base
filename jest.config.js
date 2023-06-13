064bdd7d51eb142ea949efb7e72dec2f58c2cb0c
Julian Insua <jii.julious@gmail.com>
2023-06-13 11:12:35 +0200

    Roll back to ES5 require imports for jest config file. Also added a module mapper and module directories so that the imports are "found" when testing.

--- jest.config.js
+++ jest.config.js
@@ -1,2 +1,10 @@
-export const preset = 'ts-jest'
-export const testEnvironment = 'node'
+const { resolve } = require('path')
+
+module.exports = {
+	preset: 'ts-jest',
+	testEnvironment: 'node',
+	moduleDirectories: ['node_modules', 'src'],
+	moduleNameMapper: {
+		'^src/(.*)$': resolve(__dirname, './src/$1'),
+	},
+}
